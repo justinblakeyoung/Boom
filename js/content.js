65a59a9655c63679403616c83eb9d4885d19db2a
Justin Young <JYoung1@oceaneering.com>
2020-01-28 15:57:21 -0600

    commit

--- js/content.js
+++ js/content.js
@@ -2,8 +2,11 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
+      var studentName = document.getElementsByClassName("info-main-list")[0].getElementsByTagName('p').item(0).innerHTML;
+      var sn = document.getElementsByClassName("item-doc is-active").item(0).innerText;
+
       // var myList_sn = document.getElementsByClassName("course-lessonSN");
-      // var sn = myList_sn[0].innerHTML;
+      // 
       // var serialNumber = document.getElementsByClassName("report-course-display-name")[0].innerHTML.replace("Course: ", "").replace(/\s/g, '');
       // var studentName = document.getElementsByClassName("report-student-display-name")[0].innerHTML;
       // var reg = new RegExp(studentName, "g");
@@ -20,8 +23,23 @@ chrome.runtime.onMessage.addListener(
 
       // var url = "http://mean-stack-guide-tmzmh.run.goorm.io/copy/" + encodeURIComponent(JSON.stringify(sendObject));
       var currentUrl = window.location.href;
-      var url = "http://mean-stack-guide-tmzmh.run.goorm.io/getInfo/" + currentUrl.substring(currentUrl.indexOf("classId"),1000);
+      var subCur = currentUrl.substring(currentUrl.indexOf("classId"),1000);
+      
 
+      var object = decodeURIComponent(subCur);
+	    var obj = object.split("&").reduce(function(prev, curr, i, arr) {
+      var p = curr.split("=");
+      prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
+      return prev;
+      }, {});
+
+      var classId = obj.classId;
+      var studentId = obj.studentId;
+      var lessonId = obj.lessonId;
+
+      var sendObject = {"serialNumber":sn,"studentName":studentName,"classId":classId,"studentId":studentId,"lessonId":lessonId}
+
+      var url = "http://mean-stack-guide-tmzmh.run.goorm.io/getInfo/" + encodeURIComponent(JSON.stringify(sendObject));
       //console.log(firstHref);
 
       // This line is new!
